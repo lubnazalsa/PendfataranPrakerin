667a0542c2d9997b099807b3032c5deb34b792f5
Lubna Zalsa <lubnazalsaa@gmail.com>
2024-05-16 08:54:47 +0700

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticable;
+
+class User extends Authenticable
+{
+    use HasFactory;
+
+    protected $table = 'tb_user';
+    protected $primaryKey = 'user_id';
+
+    protected $fillable = ['name', 'username', 'password'];
+}
---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::get('register', [UserController::class, 'register'])->name('register');
+Route::post('register', [UserController::class, 'register_action'])->name('register.action');
+Route::get('login', [UserController::class, 'login'])->name('login');
+Route::post('login', [UserController::class, 'login_action'])->name('login.action');
+
+
